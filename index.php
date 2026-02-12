7f082486a8755037895076e9c750daf2770d6df8
Yuvaraj Pandian <yuva3007raj@gmail.com>
2026-02-12 10:38:42 +0530

    Initial PHP Coolify demo with Docker

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "<h1>PHP Coolify Demo</h1>";
+echo "<p>Container is working successfully!</p>";
+
+echo "<hr>";
+
+echo "Server Info:<br>";
+echo "PHP Version: " . phpversion() . "<br>";
+echo "Server Time: " . date("Y-m-d H:i:s");
+
+?>
